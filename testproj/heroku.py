7b005871ee85dc4c472b6877d6f278fec3efc2db
Vladimir Fedorenko <vvf7723@gmail.com>
2015-03-02 13:00:37 +0300

    Heroku: static files settings"

--- testproj/heroku.py
+++ testproj/heroku.py
@@ -4,3 +4,10 @@ import dj_database_url
 DATABASES['default'] = dj_database_url.config()
 DATABASES['default']['ENGINE'] = 'django_postgrespool'
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
