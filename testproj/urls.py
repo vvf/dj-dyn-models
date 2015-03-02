5d199c62e47ae06b50b6f0698ac48aabef633be6
Vladimir Fedorenko <vvf7723@gmail.com>
2015-03-02 14:18:23 +0300

    Heroku (static problem)

--- testproj/urls.py
+++ testproj/urls.py
@@ -9,3 +9,6 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+urlpatterns += staticfiles_urlpatterns()
